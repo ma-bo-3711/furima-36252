006fbc4d67d7b26be33010cc3294fd0a626a09d3
ma-bo-3711 <detroit3711@gmail.com>
2021-08-06 21:37:52 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,21 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encrypted＿password, with: PASSWORD_REGEX
+
   validates :name,       presence: true
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :first_kana, presence: true
-  validates :last_kana,  presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
+    validates :first_kana
+    validates :last_kana
+  end
+
   validates :birth,      presence: true
 
 end
