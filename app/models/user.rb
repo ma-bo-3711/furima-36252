5b61cab6a2ed9af837f25d372194c432fd2b9835
ma-bo-3711 <detroit3711@gmail.com>
2021-08-10 14:19:49 +0900

    修正１

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,24 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
-  
-  validates :name,       presence: true
-  
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :first_name
-    validates :last_name
-  end
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
 
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/} do
-    validates :first_kana
-    validates :last_kana
+  with_options presence: true do
+    # presence:trueのみ適用されるカラムを書き出す
+      validates :name
+      validates :birth
+    
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      # 「presence:true」かつ「漢字カタカナひらがなの正規表現が適用される」カラムを書き出す 
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶ]+\z/} do
+      # 「presence:true」かつ「カタカナの正規表現が適用される」カラムを書き出す
+      validates :first_kana
+      validates :last_kana
+    end
   end
-
-  validates :birth,      presence: true
 
 end
